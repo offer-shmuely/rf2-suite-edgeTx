7af397e3f77e5ebafea3ff7717b61e141c06ed6e
shmuely <offer.shmuely@gmail.com>
2025-03-22 21:05:57 +0200

    feat(rf2_server): remove definition of arm-switch, it now happen automatically

--- SCRIPTS/RF2_touch/widgets/rf2_server_opt.lua
+++ SCRIPTS/RF2_touch/widgets/rf2_server_opt.lua
@@ -2,11 +2,11 @@
 
 local M = {
     options = {
-        { "arm_switch", SOURCE, getSwitchIndex("SF"..CHAR_UP)  },
+        -- { "arm_switch", SWITCH, getSwitchIndex("SF"..CHAR_UP)  },
         --{ "text_color", COLOR, COLOR_THEME_PRIMARY2 },
     },
     translate = {
-        arm_switch = "Arm/Safety Switch",
+        -- arm_switch = "Arm/Safety Switch",
         --text_color = "Text Color",
     },
 }
